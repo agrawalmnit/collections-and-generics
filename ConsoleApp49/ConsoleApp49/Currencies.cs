c7f2748e92f6b22ab5bd9a29d757562fd0a2882d
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2023-01-05 13:57:38 +0530

    Add files via upload

---
+++ ConsoleApp49/ConsoleApp49/Currencies.cs
@@ -0,0 +1,14 @@
+namespace ConsoleApp49;
+
+public static class Currencies
+{
+    public static Currency Usd => new("USD");
+    public static Currency Eur => new("EUR");
+    public static Currency Gbp => new("GBP");
+    public static Currency Jpy => new("JPY");
+
+   
+
+        }
+    }
+}
---
+++ ConsoleApp49/ConsoleApp49/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp49;
+
+namespace ConsoleApp49;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+        try
+        {
+            foreach (Worker worker in Workers.TestData)
+            {
+                Console.WriteLine(worker);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"ERROR: {e.Message}");
+        }
+    }
+}
